9a42bca6c84945ed93d17a8bd0682316f30ebc85
Claus Eggers Sørensen <es@forthehearts.net>
2024-11-17 16:34:38 +0100

    Added GalleyProofBX90000Zenith documents

---
+++ galleyproof/GalleyProof.js
@@ -0,0 +1,9 @@
+// =============================================================================
+// GalleyProof.js
+// =============================================================================
+
+import { DragAndDrop } from '../shared/DragAndDrop.js';
+import { FontLoader } from '../core/FontLoader.js';
+import { UIControls } from '../shared/UIControls.js';
+import { FontInfoRenderer } from '../core/FontInfo.js';
+import { VariationAxes } from './VariationAxes.js';
