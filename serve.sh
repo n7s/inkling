21f02eddec74e9572d03b7d5c162c27b46b031b3
Claus Eggers Sørensen <es@forthehearts.net>
2024-11-19 21:15:49 +0100

    Also open MacOS Chrome with the URL in the shell script

--- serve.sh
+++ serve.sh
@@ -9,6 +9,15 @@ fi
 # Start server with caching disabled and CORS enabled
 echo "Starting server with caching disabled..."
 cd http_root
-http-server -c-1 --cors --silent
+http-server -c-1 --cors --silent &
+
+# MacOS specific: Open the URL in Chrome
+# Wait for the port to become available
+while ! nc -z localhost 8080; do
+  sleep 0.1
+done
+
+open -a "Google Chrome" "http://localhost:8080/"
+wait
 
 #EOF
